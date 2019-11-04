d8eb93af286a91dec150032b4ad8b7359a99721c
Jonas Andreasson <jonas.m.andreasson@gmail.com>
2019-11-04 10:50:50 +0100

    Updates

---
+++ precache-manifest.aca952592206d1f77aebfee90464e04c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb1569baf462edbe803b673f2d37f200",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "577b2426505bba15dc4b",
+    "url": "/react-calculator/static/css/main.c110ae2d.chunk.css"
+  },
+  {
+    "revision": "06f3b9b9065ed86f0029",
+    "url": "/react-calculator/static/js/2.bb69bf23.chunk.js"
+  },
+  {
+    "revision": "577b2426505bba15dc4b",
+    "url": "/react-calculator/static/js/main.2aecde8f.chunk.js"
+  },
+  {
+    "revision": "b4f5323f520c0432ac6e",
+    "url": "/react-calculator/static/js/runtime-main.fe4d6796.js"
+  }
+]);
